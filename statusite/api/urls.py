6e84293466e1441da3e334ba56684185ad4fbeda
Chris Polcyn <cpolcyn@salesforce.com>
2017-07-12 17:06:19 -0700

    add API URL for repository API

--- statusite/api/urls.py
+++ statusite/api/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import include
 from django.conf.urls import url
 
+from statusite.repository import views as repository_views
 from statusite.youtube import views as youtube_views
 
 
 urlpatterns = [
     url(
+        r'repository/(?P<owner>\w+)/(?P<repo>[^/].*)$',
+        repository_views.api_repository.as_view(),
+        name='api-repository',
+    ),
+    url(
         r'^youtube/playlists/(?P<youtube_id>\w+)$',
         youtube_views.PlaylistView.as_view(),
-        name='playlist',
+        name='api-playlist',
     ),
 ]
