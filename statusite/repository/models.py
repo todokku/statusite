3be02294700a25c8d0702dec9953a80b76a87858
Christian Carter <christian.carter@salesforce.com>
2017-08-28 13:53:34 -0700

    basic buildresult model

--- statusite/repository/models.py
+++ statusite/repository/models.py
@@ -105,3 +105,22 @@ class Release(models.Model):
 
     def __str__(self):
         return '{}: {}'.format(self.repo.product_name, self.version)
+
+
+class BuildResult(models.Model):
+    class Meta:
+        verbose_name = 'Build Result'
+
+    repo = models.ForeignKey(Repository, related_name='build_results')
+    release = models.ForeignKey(Release, related_name='build_results')
+    plan_name = models.CharField(max_length=255)
+    mbci_build_id = models.IntegerField('mrbelvedereci build id')
+    status = models.CharField(max_length=25)
+    build_date = models.DateField()
+    tests_passed = models.IntegerField()
+    tests_failed = models.IntegerField()
+    tests_total = models.IntegerField()
+    # TODO: additional build stats
+
+    def __str__(self):
+        return '{}: {} [Build {}]'.format(self.repo.product_name, self.release.version, self.mbci_build_id)
