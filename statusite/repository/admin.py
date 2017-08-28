054765fb0780366d7131ac435ee6064cf2919c85
Christian Carter <christian.carter@salesforce.com>
2017-08-28 13:53:54 -0700

    buildresult admin

--- statusite/repository/admin.py
+++ statusite/repository/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from statusite.repository.models import Release
 from statusite.repository.models import Repository
+from statusite.repository.models import BuildResult
 
 class ReleaseAdmin(admin.ModelAdmin):
     list_display = ('repo', 'name', 'beta')
@@ -10,3 +11,7 @@ admin.site.register(Release, ReleaseAdmin)
 class RepositoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner')
 admin.site.register(Repository, RepositoryAdmin)
+
+class BuildResultAdmin(admin.ModelAdmin):
+    list_display = ('repo','release','plan_name','status')
+admin.site.register(BuildResult, BuildResultAdmin)
